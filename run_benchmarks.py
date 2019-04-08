faecffced074e03f1e4d17b18dd3e85af878c1d0
Josh Bowman <jbowman353@gmail.com>
2019-04-08 03:33:35 -0400

    Degree parsing for input system seems to be working now

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -11,13 +11,22 @@ import time, csv, sys, os, datetime, re
 def get_degree(sys_string):
     newstr = sys_string.replace(' ', '')
     degs = []
+
     for i in range(len(newstr)):
         if newstr[i] == '*' and newstr[i+1] == '*':
             j = i + 2
-            # TODO find the number part of the exponent, after **
+            deg = newstr[j]  # get first number of exponent
+            while True:
+                j += 1
+                # Check to make sure its a number
+                try:
+                    float(newstr[j])
+                except ValueError:
+                    break
+                deg += newstr[j]
+            degs.append(deg)
 
-
-    return max(degs)
+    return max(degs) if len(degs) != 0 else 1
 
 
 assert len(gb_input.inputs) != 0
@@ -62,14 +71,14 @@ for input_data in gb_input.inputs:
     # end_f5b_gpu = time.time()
     # f5b_gpu_runtime = end_f5b_gpu - start_f5b_gpu
 
-    output_data.append({'Input Name': fname, 'SymPy F5B Time': f5b_runtime, 'F5B-Like GPU Time': None,
-                        'Matching Results?': False, 'Fastest Runtime': None, 'Number of Variables': len(var_list),
-                        'Max Degree': None})
+    output_data.append({'Input Name': fname, 'SymPy F5B Time (sec)': f5b_runtime, 'F5B-Like GPU Time (sec)': None,
+                        'Matching Results?': False, 'Fastest Runtime (sec)': None, 'Number of Variables': len(var_list),
+                        'Max Degree': get_degree(sys_string)})
 
     print(fname + ' Completed!\n ----------')
     
 
 with open(output_file_path, 'w') as csv_file:
-    out_writer = csv.DictWriter(csv_file, fieldnames = output_data[0].keys())
+    out_writer = csv.DictWriter(csv_file, fieldnames=output_data[0].keys())
     out_writer.writeheader()
     out_writer.writerows(output_data)
